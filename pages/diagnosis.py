4219b049f9ef5e3e3304112b5f09a3db73634ce3
Stop engine 🚂🚒 <elnilemoon@gmail.com>
2025-04-20 23:09:30 +0200

    Update diagnosis.py

--- pages/diagnosis.py
+++ pages/diagnosis.py
@@ -11,8 +11,8 @@ st.set_page_config(page_title="تحليل الحساسات والأكواد", la
 
 # ==== تصميم العنوان ====
 st.markdown("""
-    <h1 style='text-align: center; color: #2C3E50; background-color: #F9ED69; padding: 10px; border-radius: 15px;'>
-        AI Car Diagnosis - Final Sensor-Fault Analyzer
+    <h1 style='text-align: center; color: #2C3E50; background-color: #FF0000; padding: 10px; border-radius: 15px;'>
+        AI Car Diagnosis -    Sensor-Fault code
     </h1>
 """, unsafe_allow_html=True)
 
