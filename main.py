87cb28fa871d63a66f80717acc2a7f144727f546
Stop engine 🚂🚒 <elnilemoon@gmail.com>
2025-04-20 17:08:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#test file 
+#final project 
+
+import streamlit as st
+
+st.set_page_config(page_title="تشخيص الأعطال", layout="wide")
+
+st.sidebar.title("قائمة التنقل")
+st.sidebar.page_link("pages/landing.py", label="الصفحة الافتتاحية")
+st.sidebar.page_link("pages/diagnosis.py", label="التشخيص وتحليل البيانات")
+
+st.markdown("<h1 style='text-align: center;'>مرحبا بك في نظام Check by AI</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>يرجى اختيار صفحة من القائمة الجانبية</p>", unsafe_allow_html=True)
+
