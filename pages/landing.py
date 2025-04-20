72050b7cf08b9ae675e8cb629acb9de5932d8320
Stop engine 🚂🚒 <elnilemoon@gmail.com>
2025-04-20 23:26:51 +0200

    Update landing.py

--- pages/landing.py
+++ pages/landing.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="الصفحة الافتتاحية", layout="wide"
 st.markdown(
     """
     <div style='text-align:center; padding:40px; background: linear-gradient(90deg, #e60000, #000); color:white; border-radius:15px;'>
-        <h1>مرحباً بك في تطبيق Check by AI</h1>
+        <h1 مرحبا   Check by AI</h1>
         <h3>نظام ذكي لتحليل الأعطال والتنبؤ بها باستخدام بيانات الحساسات</h3>
         <br>
         <p style='font-size:20px;'>يمكنك بدء التشخيص من القائمة الجانبية</p>
