2ba56565323d2b13d31b70070ef22fac227ef073
Stop engine 🚂🚒 <elnilemoon@gmail.com>
2025-05-20 20:07:23 +0300

    Update predict_fault.py

--- pages/predict_fault.py
+++ pages/predict_fault.py
@@ -1,4 +1,4 @@
-pages/predict_fault_final.py
+#pages/predict_fault_final.py
 
 import streamlit as st import pandas as pd import numpy as np import matplotlib.pyplot as plt import seaborn as sns import joblib import base64
 
